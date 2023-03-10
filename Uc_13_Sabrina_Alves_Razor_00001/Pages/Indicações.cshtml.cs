947210a92b6a0c5c521ae7f034ef6142ea0baf17
SabrinaMAlves <114673380+SabrinaMAlves@users.noreply.github.com>
2023-03-10 11:16:35 -0300

    Adicionar arquivos de projeto.

---
+++ Uc_13_Sabrina_Alves_Razor_00001/Pages/Indicações.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Uc_13_Sabrina_Alves_Razor_00001.Pages
+{
+    public class IndicaçõesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Uc_13_Sabrina_Alves_Razor_00001/Pages/Vazio.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Uc_13_Sabrina_Alves_Razor_00001.Pages
+{
+    public class VazioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
